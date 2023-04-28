22c845444b3d3165af1a23ab324a9d0cde05ea05
Vichikhovska Anastasiia <vichikhovskaya.28@gmail.com>
2023-04-28 15:21:51 +0300

    first commit and last

---
+++ 29.JavaScript basics/index.js
@@ -0,0 +1,46 @@
+// easy
+
+let first_name
+first_name = "Anjela";
+
+let second_name
+second_name = "Nick";
+
+let third_name
+third_name = "Mike";
+
+let fourth_name
+fourth_name = "Gabriella";
+
+console.log (third_name);
+
+// normal
+
+let name = prompt("What is your name?");
+console.log("Hello " + name);
+alert("Hello " + name);
+
+let year = prompt("What year of your birth?");
+console.log(year);
+alert (year);
+
+const data = ("2023");
+let number = prompt("Is that your age?", (data - year));
+console.log(data - year);
+
+alert("imagine a square and guess what the side length of this square will be. let's find its perimeter.");
+let message = prompt("enter the length of the side of the square");
+let P = alert((message)*4);
+console.log(P);
+
+let GB = alert("Good job! Have a nice day!");
+console.log(GB);
+
+// max
+
+const euro = ("40.26");
+const doll = ("36.57");
+let question = alert("Do you want know how much money you have in dollars?");
+let calc = prompt ("Write how much euro do you have");
+let number2 = alert ((euro)*calc /(doll));
+let joke = alert ("Now I too know how much money you have");
