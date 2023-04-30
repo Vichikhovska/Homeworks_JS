296d16e65c71d8bae2b38787bb99180316c8cf0c
Vichikhovska Anastasiia <vichikhovskaya.28@gmail.com>
2023-04-30 18:51:33 +0300

    fixed bugs

--- 29.JavaScript basics/index.js
+++ 29.JavaScript basics/index.js
@@ -1,46 +1,38 @@
 // easy
 
-let first_name
-first_name = "Anjela";
+const firstName = "Anjela";
+const secondName = "Nick";
+const thirdName = "Mike";
+const fourthName = "Gabriella";
 
-let second_name
-second_name = "Nick";
-
-let third_name
-third_name = "Mike";
-
-let fourth_name
-fourth_name = "Gabriella";
-
-console.log (third_name);
+console.log (thirdName);
 
 // normal
 
-let name = prompt("What is your name?");
+const name = prompt("What is your name?");
 console.log("Hello " + name);
 alert("Hello " + name);
 
-let year = prompt("What year of your birth?");
+const year = prompt("What year of your birth?");
 console.log(year);
 alert (year);
 
-const data = ("2023");
-let number = prompt("Is that your age?", (data - year));
+const data = 2023;
+const number = prompt("Is that your age?", (data - year));
 console.log(data - year);
 
 alert("imagine a square and guess what the side length of this square will be. let's find its perimeter.");
-let message = prompt("enter the length of the side of the square");
-let P = alert((message)*4);
+const message = prompt("enter the length of the side of the square");
+alert((message)*4);
 console.log(P);
 
-let GB = alert("Good job! Have a nice day!");
-console.log(GB);
+alert("Good job! Have a nice day!");
 
 // max
 
-const euro = ("40.26");
-const doll = ("36.57");
-let question = alert("Do you want know how much money you have in dollars?");
+const euro = 40.26;
+const doll = 36.57;
+alert("Do you want know how much money you have in dollars?");
 let calc = prompt ("Write how much euro do you have");
-let number2 = alert ((euro)*calc /(doll));
-let joke = alert ("Now I too know how much money you have");
+alert ((euro)*calc /(doll));
+alert ("Now I too know how much money you have");
